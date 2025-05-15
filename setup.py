39c6a363ead86736997801dea161de2f507ae5f8
Tijs Alleman <tijs.alleman@ugent.be>
2025-05-15 18:12:57 -0400

    update pyproject.toml

--- setup.py
+++ setup.py
@@ -1,51 +1,39 @@
-from setuptools import find_packages, setup, Extension
+from setuptools import setup, Extension
+import platform
 import pybind11
 
-# boost installation paths
-import platform
-if platform.system() == 'Darwin':  # macOS
-    boost_include_dir = '/opt/homebrew/Cellar/boost/1.87.0_1/include'
-    boost_lib_dir = '/opt/homebrew/Cellar/boost/1.87.0_1/lib'
-elif platform.system() == 'Linux':  # Ubuntu in GitHub Actions
-    boost_include_dir = '/usr/include'  # Default path for Boost headers
-    boost_lib_dir = '/usr/lib/x86_64-linux-gnu'  # Default path for Boost libraries
-else:
-    raise RuntimeError("Unsupported OS")
+# This setup file contains the dynamic parts of `hierarchSIR` installer, while `pyproject.toml` contains the static parts
 
-# point toward C++ model
+# find the C++ Boost integration module
+def find_boost_include():
+    if platform.system() == 'Darwin':
+        return '/opt/homebrew/Cellar/boost/1.87.0_1/include'
+    elif platform.system() == 'Linux':
+        return '/usr/include'
+    else:
+        raise RuntimeError("Unsupported OS")
+
+def find_boost_lib():
+    if platform.system() == 'Darwin':
+        return '/opt/homebrew/Cellar/boost/1.87.0_1/lib'
+    elif platform.system() == 'Linux':
+        return '/usr/lib/x86_64-linux-gnu'
+    else:
+        raise RuntimeError("Unsupported OS")
+
+# define the C++ module we want to bind to Python
 ext_modules = [
     Extension(
         "hierarchSIR.sir_model",
-        ["src/hierarchSIR/sir_model.cpp"],
-        include_dirs=[
-            pybind11.get_include(),
-            boost_include_dir,  # Add Boost include path
-        ],
-        libraries=["boost_system"],  # Link Boost
-        library_dirs=[boost_lib_dir],  # Add Boost library path
+        sources=["src/hierarchSIR/sir_model.cpp"],
+        include_dirs=[pybind11.get_include(), find_boost_include()],
+        library_dirs=[find_boost_lib()],
+        libraries=["boost_system"],
         language="c++",
-        extra_compile_args=["-std=c++14"],  # Ensure C++14 support
-    ),
+        extra_compile_args=["-std=c++14"]
+    )
 ]
 
-# installs the Python package
 setup(
-    name='hierarchSIR',
-    packages=find_packages("src", exclude=["*.tests"]),
-    package_dir={'': 'src'},
-    version='0.0',
-    description='A multi-strain SIR seasonal influenza model',
-    author='Dr. Tijs W. Alleman, Johns Hopkins University, Cornell University',
-    license='CC-BY-NC-SA',
-    install_requires=['matplotlib', 'xarray', 'numpy', 'pandas', 'scipy', 'openpyxl', 'pytest', 'pySODM>=0.2.8'],
-    ext_modules=ext_modules,
-    python_requires='>3.12.0',
-    extras_require={
-        "develop":  ["pytest"]
-    }
+    ext_modules=ext_modules
 )
-
-
-
-
-
