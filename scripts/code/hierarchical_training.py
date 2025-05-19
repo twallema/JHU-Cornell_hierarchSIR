251da7822759fce569d55b722ae1204c017cede8
Tijs Alleman <tijs.alleman@ugent.be>
2025-05-19 11:07:25 -0400

    training file

--- scripts/code/hierarchical_training.py
+++ scripts/code/hierarchical_training.py
@@ -40,9 +40,9 @@ fips_state = 37
 
 # calibration settings
 ## datasets
-identifiers_list = ['exclude_None',]                                                                                # identifiers of training datasets
+identifiers_list = ['exclude_2023-2024',]                                                                                # identifiers of training datasets
 seasons_list = [                                                                                                    # season to include in training
-        ['2014-2015', '2015-2016', '2016-2017', '2017-2018', '2018-2019', '2019-2020', '2023-2024', '2024-2025'],
+        ['2014-2015', '2015-2016', '2016-2017', '2017-2018', '2018-2019', '2019-2020', '2024-2025'],
         ]                                                                                                                   
 start_calibration_month = 10                                                                                        # start calibration on month 10, day 1
 end_calibration_month = 5                                                                                           # end calibration on month 5, day 1
@@ -50,9 +50,9 @@ run_date = datetime.today().strftime("%Y-%m-%d")
 ## define number of chains
 max_n = 100000
 pert = 0.05
-processes = int(os.environ.get('NUM_CORES', '16'))
+processes = 3 #int(os.environ.get('NUM_CORES', '16'))
 ## printing and postprocessing
-print_n = 10000
+print_n = 5000
 backend = None
 discard = 0
 thin = 100
