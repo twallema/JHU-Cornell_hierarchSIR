98d87496f9c211149bf2d022a5fa8221685c9a83
twallema <twa27@cornell.edu>
2025-08-05 15:44:06 -0400

    latest compute accuracy script

--- scripts/manuscript/compute_accuracy.py
+++ scripts/manuscript/compute_accuracy.py
@@ -22,7 +22,7 @@ MY_FOLDER
 import os
 import numpy as np
 import pandas as pd
-from scipy.stats import gmean
+from scipy.stats import norm, rankdata, gmean
 from datetime import datetime, timedelta
 from hierarchSIR.utils import get_NC_influenza_data
 from hierarchSIR.accuracy import compute_WIS
@@ -41,13 +41,14 @@ start_month = 11
 start_day = 15
 end_month = 4
 end_day = 7
+seasons_exclude = []
 
 # finding the right simulations
 model_name_overall = 'JHU_IDD-hierarchSIM'
 location = '37'                                             # NC FIPS code
 model_names = get_subfolders(os.path.dirname(__file__))
-immunity_linking = [False, True]
-ED_visits = [False, True]
+immunity_linking = [False, ]
+ED_visits = [True,]
 
 WIS_collection = []
 print('Starting loop...')
@@ -60,67 +61,68 @@ for mn in model_names:
             print(f'\t\t\tED Visits: {ev}')
             seasons = get_subfolders(os.path.join(os.path.dirname(__file__), f'{mn}/immunity_linking-{il}/ED_visits-{ev}'))
             for season in seasons:
-                # derive start of season year
-                season_start = int(season[:4])
-                # fetch hyperparameters
-                hyperparameters = get_subfolders(os.path.join(os.path.dirname(__file__), f'{mn}/immunity_linking-{il}/ED_visits-{ev}/{season}'))
-                for hp in hyperparameters:
-                    # get all enddates of forecasts in a given season from the folder names
-                    subdirectories = get_subfolders(os.path.join(os.path.dirname(__file__), f'{mn}/immunity_linking-{il}/ED_visits-{ev}/{season}/{hp}'))
-                    reference_dates = [datetime.strptime(subdir[15:], '%Y-%m-%d') for subdir in subdirectories]
-                    data_ends = [datetime.strptime(subdir[15:], '%Y-%m-%d')-timedelta(weeks=1) for subdir in subdirectories] 
-                    # only evaluate between user-supplied start and enddate
-                    filtered_subdirectories = []
-                    filtered_reference_dates = []
-                    for subdir, data_end, reference_date in zip(subdirectories, data_ends, reference_dates):
-                        if datetime(season_start, start_month, start_day) <= data_end <= datetime(season_start+1, end_month, end_day):
-                            filtered_subdirectories.append(subdir)
-                            filtered_reference_dates.append(reference_date)
-                    subdirectories = filtered_subdirectories
-                    reference_dates = filtered_reference_dates
-                    # loop over directories to collect groundtruth, forecasts and baseline model WIS
-                    datas = []
-                    simouts = []
-                    baselines = []
-                    for subdir, reference_date in zip(subdirectories, reference_dates):
-                        ## get forecast
-                        tmp = pd.read_csv(os.path.join(os.path.dirname(__file__), f'{mn}/immunity_linking-{il}/ED_visits-{ev}/{season}/{hp}/{subdir}/{reference_date.date()}-{model_name_overall}.csv'), parse_dates=True, date_format='%Y-%m-%d')
-                        ## slice right location and target
-                        tmp = tmp[((tmp['location'] == int(location)) & (tmp['target'] == 'wk inc flu hosp'))]
-                        ## sum over strains and assign to Hubverse 'value' column --> insert copula here
-                        column_names_sum = [f'strain_{i}' for i in range(n_strains)]
-                        tmp['value'] = tmp[column_names_sum].sum(axis=1)
-                        ## convert to quantiles if this was not available yet
-                        if 'quantiles' not in tmp['output_type'].unique():
-                            ## define desired quantiles
-                            q_desired = [0.01, 0.025, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 0.975, 0.99]
-                            ## compute them 
-                            group_cols = ['reference_date', 'target', 'horizon', 'location', 'target_end_date']
-                            tmp = (
-                                tmp.groupby(group_cols)['value']
-                                .quantile(q_desired)
-                                .reset_index()
-                            )
-                            ## The 'level_5' column contains the quantile levels from the groupby-quantile
-                            tmp = tmp.rename(columns={'level_5': 'output_type_id', 'value': 'value'})
-                            tmp['output_type'] = 'quantile'
-                            ## Optional: reorder columns like original
-                            desired_col_order = ['reference_date', 'target', 'horizon', 'location',
-                                                'output_type', 'output_type_id', 'target_end_date', 'value']
-                            tmp = tmp[desired_col_order]
-                        ## make sure ref date is datetime
-                        tmp['reference_date'] = pd.to_datetime(tmp['reference_date'])
-                        tmp['target_end_date'] = pd.to_datetime(tmp['target_end_date'])
-                        ## append to list
-                        simouts.append(tmp)
-                        ## get groundthruth data (+ the forecast horizon of four weeks)
-                        data = get_NC_influenza_data(reference_date+timedelta(weeks=-1), reference_date+timedelta(weeks=3), season)['H_inc']*7
-                        datas.append(data)
-                        ## get baseline WIS scores
-                        baseline = pd.read_csv('baselineModels-accuracy.csv', parse_dates=True, date_format='%Y-%m-%d')
-                        baseline['reference_date'] = pd.to_datetime(baseline['reference_date'])
-                        baseline = baseline[baseline['reference_date'] == reference_date]
-                        baselines.append(baseline[['model', 'reference_date', 'horizon', 'WIS']])
+                if season not in seasons_exclude:
+                    # derive start of season year
+                    season_start = int(season[:4])
+                    # fetch hyperparameters
+                    hyperparameters = get_subfolders(os.path.join(os.path.dirname(__file__), f'{mn}/immunity_linking-{il}/ED_visits-{ev}/{season}'))
+                    for hp in hyperparameters:
+                        # get all enddates of forecasts in a given season from the folder names
+                        subdirectories = get_subfolders(os.path.join(os.path.dirname(__file__), f'{mn}/immunity_linking-{il}/ED_visits-{ev}/{season}/{hp}'))
+                        reference_dates = [datetime.strptime(subdir[15:], '%Y-%m-%d') for subdir in subdirectories]
+                        data_ends = [datetime.strptime(subdir[15:], '%Y-%m-%d')-timedelta(weeks=1) for subdir in subdirectories] 
+                        # only evaluate between user-supplied start and enddate
+                        filtered_subdirectories = []
+                        filtered_reference_dates = []
+                        for subdir, data_end, reference_date in zip(subdirectories, data_ends, reference_dates):
+                            if datetime(season_start, start_month, start_day) <= data_end <= datetime(season_start+1, end_month, end_day):
+                                filtered_subdirectories.append(subdir)
+                                filtered_reference_dates.append(reference_date)
+                        subdirectories = filtered_subdirectories
+                        reference_dates = filtered_reference_dates
+                        # loop over directories to collect groundtruth, forecasts and baseline model WIS
+                        datas = []
+                        simouts = []
+                        baselines = []
+                        for subdir, reference_date in zip(subdirectories, reference_dates):
+                            ## get forecast
+                            tmp = pd.read_csv(os.path.join(os.path.dirname(__file__), f'{mn}/immunity_linking-{il}/ED_visits-{ev}/{season}/{hp}/{subdir}/{reference_date.date()}-{model_name_overall}.csv'), parse_dates=True, date_format='%Y-%m-%d')
+                            ## slice right location and target
+                            tmp = tmp[((tmp['location'] == int(location)) & (tmp['target'] == 'wk inc flu hosp'))]
+                            ## sum over strains and assign to Hubverse 'value' column
+                            column_names_sum = [f'strain_{i}' for i in range(n_strains)]
+                            tmp['value'] = tmp[column_names_sum].sum(axis=1)
+                            ## convert to quantiles if this was not available yet
+                            if 'quantiles' not in tmp['output_type'].unique():
+                                ## define desired quantiles
+                                q_desired = [0.01, 0.025, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 0.975, 0.99]
+                                ## compute them 
+                                group_cols = ['reference_date', 'target', 'horizon', 'location', 'target_end_date']
+                                tmp = (
+                                    tmp.groupby(group_cols)['value']
+                                    .quantile(q_desired)
+                                    .reset_index()
+                                )
+                                ## The 'level_5' column contains the quantile levels from the groupby-quantile
+                                tmp = tmp.rename(columns={'level_5': 'output_type_id', 'value': 'value'})
+                                tmp['output_type'] = 'quantile'
+                                ## Optional: reorder columns like original
+                                desired_col_order = ['reference_date', 'target', 'horizon', 'location',
+                                                    'output_type', 'output_type_id', 'target_end_date', 'value']
+                                tmp = tmp[desired_col_order]
+                            ## make sure ref date is datetime
+                            tmp['reference_date'] = pd.to_datetime(tmp['reference_date'])
+                            tmp['target_end_date'] = pd.to_datetime(tmp['target_end_date'])
+                            ## append to list
+                            simouts.append(tmp)
+                            ## get groundthruth data (+ the forecast horizon of four weeks)
+                            data = get_NC_influenza_data(reference_date+timedelta(weeks=-1), reference_date+timedelta(weeks=3), season)['H_inc']*7
+                            datas.append(data)
+                            ## get baseline WIS scores
+                            baseline = pd.read_csv('baselineModels-accuracy.csv', parse_dates=True, date_format='%Y-%m-%d')
+                            baseline['reference_date'] = pd.to_datetime(baseline['reference_date'])
+                            baseline = baseline[baseline['reference_date'] == reference_date]
+                            baselines.append(baseline[['model', 'reference_date', 'horizon', 'WIS']])
 
                     # make a dataframe for the output of the season
                     idx = pd.MultiIndex.from_product([[mn,], [il], [ev], [season], [hp,], reference_dates, range(-1,prediction_horizon_weeks)], names=['model', 'immunity_linking', 'ED_visits', 'season', 'hyperparameters', 'reference_date', 'horizon'])
@@ -142,6 +144,54 @@ output = output.reset_index()
 output = output[output['horizon'] != -1]
 output = output.set_index(['model', 'immunity_linking', 'ED_visits',  'season', 'hyperparameters', 'reference_date', 'horizon'])
 
-# output to csv
+# Save output to a .csv
 output.to_csv('accuracy.csv')
-output.groupby(by=['model', 'immunity_linking', 'ED_visits']).median().to_csv('accuracy_summary.csv')
+
+# Reset index to simplify
+df_reset = output.reset_index()
+# Columns to aggregate
+cols = ['WIS', 'relative_WIS_nodrift', 'relative_WIS_drift']
+group_keys = ['model', 'immunity_linking', 'ED_visits']
+# Drop NaNs
+df_clean = df_reset.dropna(subset=cols)
+# Group by model/immunity_linking/ED_visits
+grouped = df_clean.groupby(group_keys)
+# Compute mean
+mean_df = grouped[cols].mean()
+# Compute gmean
+gmean_df = grouped[cols].agg(lambda x: gmean(np.array(x, dtype=float)))
+# Add 'mean' and 'gmean' as new first-level column index
+mean_df.columns = pd.MultiIndex.from_tuples([('mean', col) for col in mean_df.columns])
+gmean_df.columns = pd.MultiIndex.from_tuples([('gmean', col) for col in gmean_df.columns])
+# Combine them along columns
+summary_df = pd.concat([mean_df, gmean_df], axis=1)
+# Sort by column levels: ('mean', 'WIS'), ('gmean', 'WIS'), etc.
+summary_df = summary_df.sort_index(axis=1, level=0)
+summary_df = summary_df.round(2)
+# Optional: save
+summary_df.to_csv("accuracy_summary.csv")
+
+
+# Reset index to simplify
+df_reset = output.reset_index()
+# Columns to aggregate
+cols = ['WIS', 'relative_WIS_nodrift', 'relative_WIS_drift']
+group_keys = ['model', 'immunity_linking', 'ED_visits', 'season']
+# Drop NaNs
+df_clean = df_reset.dropna(subset=cols)
+# Group by model/immunity_linking/ED_visits
+grouped = df_clean.groupby(group_keys)
+# Compute mean
+mean_df = grouped[cols].mean()
+# Compute gmean
+gmean_df = grouped[cols].agg(lambda x: gmean(np.array(x, dtype=float)))
+# Add 'mean' and 'gmean' as new first-level column index
+mean_df.columns = pd.MultiIndex.from_tuples([('mean', col) for col in mean_df.columns])
+gmean_df.columns = pd.MultiIndex.from_tuples([('gmean', col) for col in gmean_df.columns])
+# Combine them along columns
+summary_df = pd.concat([mean_df, gmean_df], axis=1)
+# Sort by column levels: ('mean', 'WIS'), ('gmean', 'WIS'), etc.
+summary_df = summary_df.sort_index(axis=1, level=0)
+summary_df = summary_df.round(2)
+# Optional: save
+summary_df.to_csv("accuracy_summary_season.csv")
