b6c471b8e7283928588d9e89a013c000742bd51c
twallema <tallema2@jh.edu>
2025-09-29 15:47:14 -0400

    little bit more states

--- scripts/operational/forecast.py
+++ scripts/operational/forecast.py
@@ -27,7 +27,7 @@ from hierarchSIR.utils import initialise_model, simout_to_hubverse, plot_fit, ma
 ##############
 
 # skip fips_state
-skip_fips = [4, 5, 6, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 44, 45, 46, 47, 48, 49, 50, 51, 53, 54, 55, 56, 72]
+skip_fips = [6, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 44, 45, 46, 47, 48, 49, 50, 51, 53, 54, 55, 56, 72]
 
 # define hyperparameters to use
 hyperparameters = 'exclude_None'
