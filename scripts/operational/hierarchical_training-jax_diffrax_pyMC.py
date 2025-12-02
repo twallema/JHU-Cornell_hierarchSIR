a86b24b0305cd38c55c5e979dcb305108766dc06
twallema <tallema2@jh.edu>
2025-12-02 11:48:16 -0500

    be sure to save results

--- scripts/operational/hierarchical_training-jax_diffrax_pyMC.py
+++ scripts/operational/hierarchical_training-jax_diffrax_pyMC.py
@@ -369,8 +369,8 @@ vjp_sol_op = VJPSolOp(args_static)
 with pm.Model() as model:
 
     # Hyperparameters
-    beta_mu = pm.Normal("beta_mu", mu=0.455, sigma=0.055/2)
-    beta_sigma = pm.HalfNormal('beta_sigma', sigma=0.055/2)
+    beta_mu = pm.Normal("beta_mu", mu=0.455, sigma=0.01)
+    beta_sigma = pm.HalfNormal('beta_sigma', sigma=0.01)
     rho_h_mu = pm.HalfNormal('rho_h_mu', sigma=1e-2/3)
     rho_h_sigma = pm.HalfNormal('rho_h_sigma', sigma=1/3)
     f_I_mu = pm.HalfNormal('f_I_mu', sigma=1e-3)
@@ -390,13 +390,29 @@ with pm.Model() as model:
     # baseline variance (positive)
     omega = pm.HalfNormal("omega", sigma=0.05)
 
-    # Reparameterize constrained (0,1) params on unconstrained real line
-    psi_raw = pm.Normal("psi_raw", mu=0.0, sigma=1.0)
-    psi = pm.Deterministic("psi", pm.math.sigmoid(psi_raw))
-    s_raw   = pm.Normal("s_raw", mu=0.0, sigma=1.0)   # persistence raw
-    rho_raw = pm.Normal("rho_raw", mu=0.0, sigma=1.0) # split raw
-    s   = pm.Deterministic("s", pm.math.sigmoid(s_raw))
-    rho = pm.Deterministic("rho", pm.math.sigmoid(rho_raw))
+    # partially pooled psi
+    psi_mu_raw = pm.Normal("psi_mu_raw", mu=0.0, sigma=1.0)
+    psi_sigma_raw = pm.HalfNormal("psi_sigma_raw", sigma=1/3)
+    psi_raw_season = pm.Normal("psi_raw_season", mu=psi_mu_raw, sigma=psi_sigma_raw, shape=n_seasons)
+    psi = pm.Deterministic("psi", pm.math.sigmoid(psi_raw_season))
+    psi_mu = pm.Deterministic("psi_mu", pm.math.sigmoid(psi_mu_raw))
+    psi_sigma = pm.Deterministic("psi_sigma", pm.math.sigmoid(psi_sigma_raw))
+
+    # partially pooled s
+    s_mu_raw = pm.Normal("s_mu_raw", mu=0.0, sigma=1.0)
+    s_sigma_raw = pm.HalfNormal("s_sigma_raw", sigma=1/3)
+    s_raw_season = pm.Normal("s_raw_season", mu=s_mu_raw, sigma=s_sigma_raw, shape=n_seasons)
+    s = pm.Deterministic("s", pm.math.sigmoid(s_raw_season))
+    s_mu = pm.Deterministic("s_mu", pm.math.sigmoid(s_mu_raw))
+    s_sigma = pm.Deterministic("s_sigma", pm.math.sigmoid(s_sigma_raw))
+
+    # partially pooled rho
+    rho_mu_raw = pm.Normal("rho_mu_raw", mu=0.0, sigma=1.0)
+    rho_sigma_raw = pm.HalfNormal("rho_sigma_raw", sigma=1/3)
+    rho_raw_season = pm.Normal("rho_raw_season", mu=rho_mu_raw, sigma=rho_sigma_raw, shape=n_seasons)
+    rho = pm.Deterministic("rho", pm.math.sigmoid(rho_raw_season))
+    rho_mu = pm.Deterministic("rho_mu", pm.math.sigmoid(rho_mu_raw))
+    rho_sigma = pm.Deterministic("rho_sigma", pm.math.sigmoid(rho_sigma_raw))
 
     # GARCH coefficients in (0,1) and a_garch + b_garch = s (total persistence)
     a_garch = pm.Deterministic("a_garch", s * rho)
@@ -472,7 +488,8 @@ with pm.Model() as model:
 # ~~~~~~~~~~~~~~~~~
 
 with model:
-    trace = pm.sample(25, tune=25, chains=1, init='adapt_diag', cores=1, progressbar=True, initvals=1*[{'alpha': 0.01, 'eta': eta_opt, 'beta': beta_opt, 'mu_t': np.mean(delta_beta_opt, axis=0), 'rho_h': rho_h_opt, 'f_I': f_I_opt, 'f_R': f_R_opt},])
+    trace = pm.sample(250, tune=50, chains=4, init='adapt_diag', target_accept=0.9, cores=1, progressbar=True,
+                      initvals=4*[{'alpha': 0.01, 'eta': eta_opt, 'beta': beta_opt, 'mu_t': np.mean(delta_beta_opt, axis=0), 'rho_h': rho_h_opt, 'f_I': f_I_opt, 'f_R': f_R_opt},])
 
 # Generate traces
 variables2plot = [
@@ -481,8 +498,9 @@ variables2plot = [
                 'f_R_a', 'f_R_b', 'f_R',                            # f_R
                 'f_I_mu', 'f_I_sigma', 'f_I',                       # f_I
                 'beta_mu', 'beta_sigma', 'beta',                    # beta
-                'omega', 'a_garch', 'b_garch', 'psi', 'sigma20',    # AR-GARCH
-                'mu_t', 'delta0', 's', 'rho'
+                'omega', 'a_garch', 'b_garch', 'sigma20', 'delta0', # AR-GARCH
+                's', 's_mu', 's_sigma', 'rho', 'rho_mu', 'rho_sigma', 'psi', 'psi_mu', 'psi_sigma',
+                'mu_t', 
                 ]
 
 # Save traces
@@ -493,7 +511,7 @@ for var in variables2plot:
     plt.close()
 
 # Build pair plots
-arviz.plot_pair(trace, var_names=["s", "rho", "psi"])
+arviz.plot_pair(trace, var_names=["s_mu", "rho_mu", "psi_mu"], divergences=True)
 plt.savefig('trace/pairplot-ARGARCH.pdf')
 plt.close()
 
@@ -505,6 +523,9 @@ plt.close()
 with model:
     posterior_predictive = pm.sample_posterior_predictive(trace)
 
+# Save traces and posterior predictive
+arviz.to_netcdf(trace, "trace/trace.nc")
+arviz.to_netcdf(posterior_predictive, "trace/posterior_predictive.nc")
 
 # Visualise modifier trajectories
 fig,ax=plt.subplots(figsize=(8.3, 11.7/5))
